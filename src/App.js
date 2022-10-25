e6132b23ec45533d47f4aa25ce60525340bd3f23
Fransérgio Gonçalves <fransergiogj7@gmail.com>
2022-10-25 18:09:28 -0300

    dev Dependeces installed - Initial project

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import {Download, Features, SectionWrapper} from "./components";
+import assets from "./assets"
 
-function App() {
+
+
+
+const App =()=> {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <Download/>
+    <Features/>
+    <SectionWrapper
+    title="You own store of Nifty NFTs. Start Sell & Growing."
+    description="Buy, store, collect NFTs, exchange & earn crypto. Join 25+ million people using ProNef Marketplace"
+    showBtn
+    mockupImg={assets.homeHero}
+    banner="banner"
+    />
+    </>
   );
 }
 
---
+++ src/components/index.js
@@ -0,0 +1,13 @@
+import Download from "./Download";
+import Features from "./Features";
+import Button from "./Button";
+import SectionWrapper from "./SectionWrapper";
+
+
+
+export {
+    Download,
+    Features,
+    Button,
+    SectionWrapper
+}
